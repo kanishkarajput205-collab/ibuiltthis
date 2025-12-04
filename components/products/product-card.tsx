fded1b1968266f722093d8bab5ab332296ec4727
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2025-12-05 00:35:15 +0100

    upvote downvote buttons

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -5,13 +5,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { ChevronDownIcon, ChevronUpIcon, StarIcon } from "lucide-react";
+import { products } from "@/db/schema";
+import { InferSelectModel } from "drizzle-orm";
+import { StarIcon } from "lucide-react";
 import Link from "next/link";
 import { Badge } from "../ui/badge";
-import { Button } from "../ui/button";
-import { InferSelectModel } from "drizzle-orm";
-import { products } from "@/db/schema";
+import VotingButtons from "./voting-buttons";
 
 type Product = InferSelectModel<typeof products>;
 
@@ -37,35 +36,11 @@ export default function ProductCard({ product }: { product: Product }) {
               <CardDescription>{product.description}</CardDescription>
             </div>
             {/** Voting buttons */}
-            <div className="flex flex-col items-center gap-1 shrink-0">
-              <Button
-                variant="ghost"
-                size="icon-sm"
-                className={cn(
-                  "h-8 w-8 text-primary ",
-                  hasVoted
-                    ? "bg-primary/10 text-primary hover:bg-primary/20"
-                    : "hover:bg-primary/10 hover:text-primary"
-                )}
-              >
-                <ChevronUpIcon className="size-5" />
-              </Button>
-              <span className="text-sm font-semibold transition-colors text-foreground">
-                {product.voteCount}
-              </span>
-              <Button
-                variant="ghost"
-                size="icon-sm"
-                className={cn(
-                  "h-8 w-8 text-primary ",
-                  hasVoted
-                    ? "hover:text-destructive"
-                    : "opacity-50 cursor-not-allowed"
-                )}
-              >
-                <ChevronDownIcon className="size-5" />
-              </Button>
-            </div>
+            <VotingButtons
+              hasVoted={hasVoted}
+              voteCount={product.voteCount}
+              productId={product.id}
+            />
           </div>
         </CardHeader>
         <CardFooter>
--- lib/products/product-actions.ts
+++ lib/products/product-actions.ts
@@ -6,6 +6,8 @@ import { db } from "@/db";
 import { products } from "@/db/schema";
 import z from "zod";
 import { FormState } from "@/types";
+import { eq, sql } from "drizzle-orm";
+import { refresh, revalidatePath } from "next/cache";
 
 export const addProductAction = async (
   prevState: FormState,
@@ -88,6 +90,92 @@ export const addProductAction = async (
       success: false,
       errors: undefined,
       message: "Failed to submit product",
+    };
+  }
+};
+
+export const upvoteProductAction = async (productId: number) => {
+  try {
+    const { userId, orgId } = await auth();
+
+    if (!userId) {
+      console.log("User not signed in");
+      return {
+        success: false,
+        message: "You must be signed in to submit a product",
+      };
+    }
+
+    if (!orgId) {
+      console.log("User not a member of an organization");
+      return {
+        success: false,
+        message: "You must be a member of an organization to submit a product",
+      };
+    }
+
+    await db
+      .update(products)
+      .set({
+        voteCount: sql`GREATEST(0, vote_count + 1)`,
+      })
+      .where(eq(products.id, productId));
+
+    revalidatePath("/");
+
+    return {
+      success: true,
+      message: "Product upvoted successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to upvote product",
+      voteCount: 0,
+    };
+  }
+};
+
+export const downvoteProductAction = async (productId: number) => {
+  try {
+    const { userId, orgId } = await auth();
+
+    if (!userId) {
+      console.log("User not signed in");
+      return {
+        success: false,
+        message: "You must be signed in to submit a product",
+      };
+    }
+
+    if (!orgId) {
+      console.log("User not a member of an organization");
+      return {
+        success: false,
+        message: "You must be a member of an organization to submit a product",
+      };
+    }
+
+    await db
+      .update(products)
+      .set({
+        voteCount: sql`GREATEST(0, vote_count - 1)`,
+      })
+      .where(eq(products.id, productId));
+
+    revalidatePath("/");
+
+    return {
+      success: true,
+      message: "Product downvoted successfully",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to downvote product",
+      voteCount: 0,
     };
   }
 };
